a1a3ee46cb623cc4508a124cfcf7131a476aefd4
isaac.calderon <isaaccalderon89@gmail.com>
2023-02-03 21:45:44 +0100

    Cambios

--- src/Joel/Joel.java
+++ src/Joel/Joel.java
@@ -7,6 +7,10 @@ public class Joel {
 		
 		System.out.println("Hola, soy Joel, el hermano pequeño de Isaac y he tenido que volver a subir esto por cafre");
 
+		System.out.println("\nHola, soy Isaac y voy a decir las tres comidas favoritas de mi bro:"
+				+ "\n1. La pizza BBQ"
+				+ "\n2. La piña con queso"
+				+ "\n3. Filetes de viento");
 	}
 
 }
