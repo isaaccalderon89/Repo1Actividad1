d352f02b8c10eccb915c59c8218a5f90c5875e6e
isaac.calderon <isaaccalderon89@gmail.com>
2023-02-03 21:11:30 +0100

    Segundo intento clase Joel

--- src/Joel/Joel.java
+++ src/Joel/Joel.java
@@ -3,7 +3,9 @@ package Joel;
 public class Joel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hola, soy Joel, el hermano pequeño de Isaac y he tenido que volver a subir esto por cafre");
 
 	}
 
