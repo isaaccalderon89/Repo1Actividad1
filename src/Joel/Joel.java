7bf20693232a8d79983ee61790313f53c3807b20
isaac.calderon <isaaccalderon89@gmail.com>
2023-02-03 21:30:22 +0100

    develop_2 sus 3 comidas favoritas

--- src/Joel/Joel.java
+++ src/Joel/Joel.java
@@ -6,7 +6,11 @@ public class Joel {
 		
 		
 		System.out.println("Hola, soy Joel, el hermano pequeño de Isaac y he tenido que volver a subir esto por cafre");
-
+		
+		System.out.println("\nMis tres comidas favoritas son:"
+				+ "\n1.La tortilla de patatas con cebolla"
+				+ "\n2. Las gachas"
+				+ "\n3. Los torreznos de Soria");
 	}
 
 }
